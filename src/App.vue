6a5e3e50d4de793a4025e218f8223311bb642710
rx-Yaimh <2601086756@qq.com>
2020-09-18 09:37:19 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Test msg="Welcome to Your Vue.js App"></Test>
+  </div>
+</template>
+
+<script>
+
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
+
+<style>
+
+</style>
